3ccd48fd3789a987c30a43ce16f152802b353689
unknown <lqh060406@gmail.com>
2026-06-08 15:38:22 +0700

    Add TestNG reports upload to artifact

--- src/test/java/com/lethilinh/cicd/tests/LoginTest.java
+++ src/test/java/com/lethilinh/cicd/tests/LoginTest.java
@@ -30,18 +30,18 @@ public class LoginTest {
         loginPage = new LoginPage(driver);
     }
 
-    @Test
-    public void testSuccessfulLogin() {
+        @Test
+    public void testStandardUserLogin() {
         loginPage.enterCredentials("standard_user", "secret_sauce");
         loginPage.clickLogin();
-        Assert.assertTrue(loginPage.isLoginSuccessful(), "Đăng nhập thất bại!");
+        Assert.assertTrue(loginPage.isLoginSuccessful(), "Đăng nhập bằng standard_user thất bại!");
     }
 
     @Test
-    public void testFailedLogin() {
-        loginPage.enterCredentials("standard_user", "wrong_password");
+    public void testProblemUserLogin() {
+        loginPage.enterCredentials("problem_user", "secret_sauce");
         loginPage.clickLogin();
-        Assert.assertFalse(loginPage.isLoginSuccessful(), "Đăng nhập đáng lẽ phải thất bại!");
+        Assert.assertTrue(loginPage.isLoginSuccessful(), "Đăng nhập bằng problem_user thất bại!");
     }
 
     @AfterMethod
